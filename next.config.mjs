e0c5d4bfeb923380adc73dfdec70980e64fc7dbf
elliott <gloubiboulga.sh@gmail.com>
2025-12-19 16:12:59 +0100

    [ADD] memory optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: false, // Désactivé pour économiser la mémoire
+    experimental: {
+        webpackMemoryOptimizations: true, // Optimisation mémoire
+    },
 };
 
 export default nextConfig;
